519d6ed6a5aa6623dacd27f347f40c81acf1a04e
Shivamvashist <ninjavashistsv1@gmail.com>
2025-01-09 19:19:27 +0530

    added CORS

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+const cors = require("cors");
 
 require('dotenv').config();
 const dbUser = process.env.DB_USER;
@@ -12,6 +13,7 @@ const { userModel, adminModel, courseModel, purchaseModel } = require("./db")
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 
 //Routes
