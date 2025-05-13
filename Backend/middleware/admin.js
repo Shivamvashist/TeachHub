009b5687042294c5a26b2afd77a955a1e1856d5b
NinjaVashist <55268923+Shivamvashist@users.noreply.github.com>
2025-05-13 19:02:16 +0530

    changed token from headers to cookies for admin

--- Backend/middleware/admin.js
+++ Backend/middleware/admin.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const { JWT_SECRET_ADMIN } = require("../config")
 
 function adminMiddleware(req,res,next){
-    const token = req.headers.token;
+    const token = req.cookies.token;
     const decoded = jwt.verify(token,JWT_SECRET_ADMIN);
 
     if(decoded){
